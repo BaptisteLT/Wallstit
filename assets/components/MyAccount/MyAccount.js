833d96ede691627c64a0a06d06979ae43d196485
Baptiste <lantranbaptiste@gmail.com>
2024-02-26 17:03:44 +0100

    improved My Account page UX

--- assets/components/MyAccount/MyAccount.js
+++ assets/components/MyAccount/MyAccount.js
@@ -16,17 +16,27 @@ const MyAccount = () => {
     return(
         <Container className="my-account-container">
             <div id="my-account-wrapper">
-                <div id="my-account-avatar-wrapper">
-                    <img src="https://lh3.googleusercontent.com/a/ACg8ocJn4wdx_5y_FWDqXQ_WX8-aw_nlZSNO2qC9kdjKCrNpGfY=s96-c" alt="avatar image" />
-                </div>
+                
+                <img src="https://lh3.googleusercontent.com/a/ACg8ocJn4wdx_5y_FWDqXQ_WX8-aw_nlZSNO2qC9kdjKCrNpGfY=s96-c" alt="avatar image" />
 
-                Edit profile
-                <div id="my-account-username-wrapper">
-                    <label htmlFor="username">Username</label>
-                    <input id="username" name="username" type='text' defaultValue='PabloEscargot' />
-                </div>
+                
+
+                
+                <span className="header-title">Edit profile</span>
+
+                
+                <label htmlFor="username">How should we call you?</label>
+                <input className="username-input" id="username" name="username" type="text" defaultValue="PabloEscargot" />
                
-                date de création du compte
+                <span className="created-at">Your account was created on 10th february 2019</span>
+
+                <button className="save-btn">Save</button>
+
+                <hr className="separator" />
+
+                <button className="delete-account-btn">Delete Account</button>
+                <span className="delete-account-text">This action is permanent!</span>
+
             </div>
         </Container>
     )
