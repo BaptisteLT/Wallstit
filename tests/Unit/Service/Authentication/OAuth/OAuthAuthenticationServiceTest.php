69c76f11f7dcbe7ecd9c4c6a55b2cebf9d4dd5cd
Baptiste <lantranbaptiste@gmail.com>
2024-03-23 14:12:46 +0100

    Added unit test for OAuthAuthenticationService

--- tests/Unit/Service/Authentication/OAuth/OAuthAuthenticationServiceTest.php
+++ tests/Unit/Service/Authentication/OAuth/OAuthAuthenticationServiceTest.php
@@ -1,44 +1,91 @@
 <?php
 namespace App\Tests\Unit\Service\Authentication\OAuth;
 
+use App\Entity\User;
+use App\Entity\Tokens\JwtToken;
+use App\Entity\Tokens\RefreshToken;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use App\Service\Authentication\Tokens\TokenManagerService;
 use App\Service\Authentication\OAuth\OAuthApi\OAuthApiInterface;
+use App\Service\Authentication\OAuth\OAuthAuthenticationService;
 use App\Service\Authentication\UserRegistration\UserManagerService;
 use App\Service\Authentication\OAuth\OAuthResponse\ResponseManagerService;
 
-final class OAuthAuthenticationService{
+class OAuthAuthenticationServiceTest extends KernelTestCase
+{
+    private UserManagerService $userManagerMock;
+    private TokenManagerService $tokenManagerMock;
+    private ResponseManagerService $responseManagerMock;
+    private OAuthAuthenticationService $oAuthAuthenticationService;
 
-    public function __construct(
-        private UserManagerService $userManager,
-        private TokenManagerService $tokenManager,
-        private ResponseManagerService $responseManager
-    ){}
+    public function setUp(): void
+    {
+        $this->userManagerMock = $this->createMock(UserManagerService::class);
+        $this->tokenManagerMock = $this->createMock(TokenManagerService::class);
+        $this->responseManagerMock = $this->createMock(ResponseManagerService::class);
+        $this->oAuthAuthenticationService = new OAuthAuthenticationService($this->userManagerMock, $this->tokenManagerMock, $this->responseManagerMock);
+    }
 
     /**
-     * Return the JWT and Refresh Token for user authentication.
-     * 
-     * @param OAuthApiInterface $OAuthApiInterface An implementation of OAuthApiInterface responsible for handling OAuth interactions.
-     * @param string $code   The authentication code received from the authentication callback.
-     * @param string $state  The state parameter used for preventing CSRF attacks.
-     *
-     * @return JsonResponse containing the JwtToken and RefreshToken in a cookie and in the response body itself.
+     * test prepareAuthenticationResponse()
      */
-    public function prepareAuthenticationResponse(OAuthApiInterface $OAuthApiInterface, string $provider, string $code, string $state): JsonResponse
+    public function testPrepareAuthenticationResponse(): void
     {
-        // Exchange the code present in the Request for a Bearer token
-        $bearerToken = $OAuthApiInterface->getBearerToken($code, $state);
-        // Retrieve user data from Google OAuth service using the Bearer token
-        $userData = $OAuthApiInterface->retrieveUserData($bearerToken);
-        // Create or update user based on retrieved data
-        $user = $this->userManager->getOrCreateUser($userData, $provider);
-        // Generate JWT token based on user
-        $jwtToken = $this->tokenManager->generateJWTToken($user);
-        // Generate the refresh token
-        $refreshToken = $this->tokenManager->generateRefreshToken($user);
+        $code = 'someCodeValue';
+        $state = 'someStateValue';
+        $provider = 'google';
+        $bearerToken = 'someBearerToken';
 
-        $response = $this->responseManager->authenticationResponse($jwtToken, $refreshToken);
+        $oAuthApiInterface = $this->createMock(OAuthApiInterface::class);
 
-        return $response;
+        //Test de la méthode getBearerToken()
+        $oAuthApiInterface->expects($this->once())
+        ->method('getBearerToken')
+        ->with($code, $state)
+        ->willReturn($bearerToken);
+
+        //Test de la méthode retrieveUserData()
+        $userData = [
+            'id' => 'some_id_value',
+            'email' => 'some_email_value',
+            'name' => 'some_name_value',
+            'picture' => 'some_picture_value',
+            'locale' => 'some_locale_value'
+        ];
+        $oAuthApiInterface->expects($this->once())
+        ->method('retrieveUserData')
+        ->with($bearerToken)
+        ->willReturn($userData);
+
+        //Test de la méthode getOrCreateUser()
+        $user = $this->createMock(User::class);
+        $this->userManagerMock->expects($this->once())
+        ->method('getOrCreateUser')
+        ->with($userData, $provider)
+        ->willReturn($user);
+
+        //Test de la méthode generateJWTToken()
+        $jwtToken = $this->createMock(JwtToken::class);
+        $this->tokenManagerMock->expects($this->once())
+        ->method('generateJWTToken')
+        ->with($user)
+        ->willReturn($jwtToken);
+  
+        //Test de la méthode generateRefreshToken()
+        $refreshToken = $this->createMock(RefreshToken::class);
+        $this->tokenManagerMock->expects($this->once())
+        ->method('generateRefreshToken')
+        ->with($user)
+        ->willReturn($refreshToken);
+
+        //Test de la méthode authenticationResponse()
+        $jsonResponse = $this->createMock(JsonResponse::class);
+        $this->responseManagerMock->expects($this->once())
+        ->method('authenticationResponse')
+        ->with($jwtToken, $refreshToken)
+        ->willReturn($jsonResponse);
+
+        $jsonResponse = $this->oAuthAuthenticationService->prepareAuthenticationResponse($oAuthApiInterface, $provider, $code, $state);
     }
 }
