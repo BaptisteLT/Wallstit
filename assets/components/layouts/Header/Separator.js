23e8e288f8cdfb4f3e03e51d3c44f3a2b554834c
Baptiste <lantranbaptiste@gmail.com>
2024-04-12 22:43:36 +0200

    Fixed navbar responsive

--- assets/components/layouts/Header/Separator.js
+++ assets/components/layouts/Header/Separator.js
@@ -3,7 +3,7 @@ import React from "react";
 function Separator()
 {
     return(
-        <div style={{height: '30px', borderLeft: '1px solid #1f1f1f', margin: '0px 30px'}}></div>
+        <div className="separator_vertical" style={{height: '30px', borderLeft: '1px solid #1f1f1f', margin: '0px 30px'}}></div>
     )
 }
 
